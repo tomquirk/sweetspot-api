fc85071c66e589eab2baf93c95be1bea6ecffce1
Tom Quirk <tomquirkacc@gmail.com>
2017-12-24 17:29:29 +1000

    init distance matrix api usecase

---
+++ README.md
@@ -0,0 +1,30 @@
+# Propi
+
+
+
+origins
+addresses
+keywords
+
+=>
+[
+  {
+    origin_location: {
+      address: "",
+      lat: 0,
+      lng: 0,
+    },
+    destinations: [
+      {
+        location: {
+          address: "",
+          lat: 0,
+          lng: 0,
+        },
+        name: "",
+        distance: 0,
+        duration: 0
+      }
+    ]
+  }
+]
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 
 const settings = require('./local_settings.js')
 
-const proximity = require('./resources/proximity')
+const addresses = require('./resources/addresses')
 
 const app = express()
 
-app.get('/v1/proximity', proximity.get)
+app.get('/v1/addresses', addresses.get)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
