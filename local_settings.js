6b0736f6fd54dfb07a2ddc00bca348b187d27a92
Tom Quirk <tomquirkacc@gmail.com>
2017-12-24 20:13:47 +1000

    rm local_settings from git

--- local_settings.js
+++
@@ -1,7 +0,0 @@
-API_KEY = 'AIzaSyCxnnuZP82AlrtMlHyM0d8oNqBIxeF08pw'
-BASE_URL = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&key=${API_KEY}`
-
-module.exports = {
-  API_KEY,
-  BASE_URL,
-}
