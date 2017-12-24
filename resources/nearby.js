2841d73707e685386470cfce1afa602ab5b980bf
Tom Quirk <tomquirkacc@gmail.com>
2017-12-24 19:59:12 +1000

    minor refactor to /nearby for multiple origins

--- resources/nearby.js
+++ resources/nearby.js
@@ -12,29 +12,61 @@ function geocode(address) {
   return axios.get(GOOGLE_GEOCODE_URL, { params })
 }
 
-function getNearby(req, res) {
-  geocode(req.query.address).then((geo_resp) => {
-    const geoData = geo_resp.data.results[0]
-    const originLocation = {
-      lat: geoData.geometry.location.lat,
-      lng: geoData.geometry.location.lng,
-      address: req.query.address,
-    }
-
-    const params = {
-      key: settings.GOOGLE_PLACES_KEY,
-      location: `${originLocation.lat},${originLocation.lng}`,
-      keyword: req.query.keyword,
-      radius: req.query.radius,
-    }
-
-    axios.get(GOOGLE_PLACES_URL, { params }).then((places_resp) => {
-      const data = {
-        origin_location: originLocation,
-        results: places_resp.data.results
+function getSingleNearby(address, options) {
+  return new Promise((resolve, reject) => {
+    console.log(address)
+    geocode(address).then((geo_resp) => {
+      const geoData = geo_resp.data.results[0]
+      console.log(geoData)
+      const originLocation = {
+        lat: geoData.geometry.location.lat,
+        lng: geoData.geometry.location.lng,
+        address: address,
       }
-      res.send(data)
+
+      const params = {
+        key: settings.GOOGLE_PLACES_KEY,
+        location: `${originLocation.lat},${originLocation.lng}`,
+        keyword: options.keyword,
+        radius: options.radius,
+      }
+
+      return axios.get(GOOGLE_PLACES_URL, { params }).then((places_resp) => {
+        const data = {
+          origin_location: originLocation,
+          results: places_resp.data.results
+        }
+
+        return resolve(data)
+      })
     })
+  })
+}
+
+function getNearby(req, res) {
+  const origins = req.query.origins.split("|")
+  const promises = origins.map(address => {
+    const options = {
+      radius: req.query.radius,
+      keyword: req.query.keyword,
+    }
+    return getSingleNearby(address, options)
+  })
+
+  Promise.all(promises).then((response) => {
+    const data = response.map(d => {
+      return {
+        origin_location: d.origin_location,
+        destinations: d.results.map((dest) => {
+          return {
+            // TODO add more stuff here
+            name: dest.name
+          }
+        }),
+      }
+    })
+
+    res.send(data)
   })
 }
 
